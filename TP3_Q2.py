786b3944bec366d2e301985610aff8e0fd9b8487
BrunoSchleder <79884743+BrunoSchleder@users.noreply.github.com>
2021-03-12 18:44:39 -0300

    Update TP3_Q2.py

--- TP3_Q2.py
+++ TP3_Q2.py
@@ -2,20 +2,23 @@
 
 #Informe a idade: 25
 #Tem obrigação de votar.
-idade = int(input("Informe a Idade "))
-while idade <= 0:
-  print("Idade Inválida")
-  idade = int(input("Informe a Idade"))
+def obrigacao_votar():
+  idade = int(input("Informe a Idade "))
+  while idade <= 0:
+    print("Idade Inválida")
+    idade = int(input("Informe a Idade"))
   
 
 #Informe a idade: 75
 #Não tem obrigação de votar.
-if idade >= 16 and idade < 18 or idade > 70:
-  print("Voto Facultativo")
+  if idade >= 16 and idade < 18 or idade > 70:
+    print("Voto Facultativo")
 
 #Informe a idade: 12
 #Não tem direito a voto.
-elif idade <16:
-  print("Não Pode Votar")
-else:
-  print("Voto Obrigatório")
+  elif idade <16:
+    print("Não Pode Votar")
+  else:
+    print("Voto Obrigatório")
+
+obrigacao_votar()
