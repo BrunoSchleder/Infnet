11015aaf61277a73d20ae8ba7ec60a214ee06379
BrunoSchleder <79884743+BrunoSchleder@users.noreply.github.com>
2021-03-30 18:29:32 -0300

    Update Assessment_Pivotto_Q5.py

--- Assessment_Pivotto_Q5.py
+++ Assessment_Pivotto_Q5.py
@@ -9,8 +9,6 @@ tabela_pib = tabela_pib.splitlines()
 head = tabela_pib[0]
 head = head.split(',')
 tabela_pib = tabela_pib[1:]
-print(head)
-print(tabela_pib)
 paises_pib = []
 anos = head[1:]
 for elemento in tabela_pib:
@@ -25,6 +23,19 @@ paises = []
 
 for linha in paises_pib:
   paises.append(linha[0])
+
+for linha in range(0, len(paises_pib)):
+  del paises_pib[linha][0]
+
+pais = input("Informe um país: ")
+if pais in paises:
+  ano = input("Informe um ano entre 2013 e 2020: ")
+  if ano in anos:
+    print(f"PIB {pais} em {ano} é de US$ {paises_pib[paises.index(pais)][anos.index(ano)]} trilhões.")
+  else:
+    print("O ano não está disponível!")
+else:
+  print("País indisponível")
   
   
 #B)
