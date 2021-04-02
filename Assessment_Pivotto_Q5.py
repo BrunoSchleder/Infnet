43f4227c69eb0deb1e88e104b7826845a1d4d580
BrunoSchleder <79884743+BrunoSchleder@users.noreply.github.com>
2021-04-02 18:58:53 -0300

    Update Assessment_Pivotto_Q5.py

--- Assessment_Pivotto_Q5.py
+++ Assessment_Pivotto_Q5.py
@@ -1,6 +1,8 @@
-#Q5
+#Questão 5
 
 #A)
+import matplotlib.pyplot as plt
+
 arquivo = open("Assessment_PIBs.csv")
 tabela_pib = arquivo.read()
 arquivo.close()
@@ -36,6 +38,26 @@ if pais in paises:
     print("O ano não está disponível!")
 else:
   print("País indisponível")
-  
-  
+
 #B)
+def variacaoperiodo(paises, anos, paises_pib):
+  for linha in range(0, 15):
+    variacao = (float(paises_pib[linha][7]) / float(paises_pib[linha][0]) -1) * 100
+    comprimento = len(paises[linha])
+    print(f"\n{paises[linha]}" + " "*(25 - comprimento) + f"Variação percentual {variacao:.2f} % no período de 2013 a 2020")
+
+variacaoperiodo(paises, anos, paises_pib)
+
+#C)
+def graf_plot(paises_pib, anos, paises):
+  pais = input("\nEscolha um país para ver o gráfico de variação do PIB (2013 a 2020): ")
+  for i in range(0, 8):
+    paises_pib[paises.index(pais)][i] = float(paises_pib[paises.index(pais)][i])
+  plt.plot(anos, paises_pib[paises.index(pais)])
+  plt.title("Desenvolvimento PIB")
+  plt.xlabel("Anos")
+  plt.ylabel("PIB em US$ Trilhões")
+  plt.ylim([min(paises_pib[paises.index(pais)]), max(paises_pib[paises.index(pais)])])
+  plt.show()
+
+graf_plot(paises_pib, anos, paises)
